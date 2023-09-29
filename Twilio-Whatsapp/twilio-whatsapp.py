fb0793cd37370b1cdbf75905ecbc115a6621c67f
adhirajpandey <87516052+adhirajpandey@users.noreply.github.com>
2023-09-29 19:25:29 +0000

    Updated dependencies

--- Twilio-Whatsapp/twilio-whatsapp.py
+++ Twilio-Whatsapp/twilio-whatsapp.py
@@ -1,25 +1,31 @@
 from twilio.rest import Client
 from flask import Flask, request
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("AUTH_TOKEN")
+
+# replace to and from numbers as per requirement
+FROM_NUMBER = 'whatsapp:+14155238886'
+TO_NUMBER = 'whatsapp:+918791335061'
 
 
-#replace account_sid and auth_token with your own credentials
-account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxx'
-auth_token = 'e09cxxxxxxxxxxxxxxxxxxxxxxx'
-
-#replace to and from numbers as per requirement
 def send_whatsapp_message(message):
     client = Client(account_sid, auth_token)
     message = client.messages.create(
-        from_='whatsapp:+14xxxxxxx',
+        from_=FROM_NUMBER,
         body=message,
-        to='whatsapp:+9187xxxxxxxx'
+        to=TO_NUMBER
     )
     return message.sid
 
-# send_whatsapp_message("Hello World")
+send_whatsapp_message("Hello World")
 
 
-#flask server to receive webhook from twilio
+# flask server to receive webhook from twilio
 app = Flask(__name__)
 
 @app.route('/twilio', methods=['POST'])
@@ -32,7 +38,7 @@ def twilio():
     datadict = {'status': status, 'tonum': tonum, 'msgid': msgid, 'message_text': Body}
 
     if status == 'received':
-        #add your logic here
+        # add your logic here
         send_whatsapp_message(datadict['message_text'])
 
     return datadict
